20a3960300a17fb392a8d88f2aa0bd7389bb15b1
SERGY <kogo_sadgi@mail.ru>
2022-06-25 21:12:53 +0300

    Zum001

---
+++ Zom001/Program.cs
@@ -0,0 +1,11 @@
+﻿// запрашиваем у пользователя число
+Console.Write("Input number: ");
+
+// создаем переменную number и сохраняем в нее значение, введение из консоли
+int number = Convert.ToInt32(Console.ReadLine());
+
+// объявляем переменую q и сохраняем в нее квадрат числа number
+int q = number * number;
+
+// вводим значения переменных number и q
+Console.WriteLine(number + " -> " + q);
