6ac68b9289d4c6e68e25034cfde0af5efd2e3845
SERGY <kogo_sadgi@mail.ru>
2022-06-25 21:14:14 +0300

    Zum003

---
+++ Zum003/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("введите день: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day == 1)
+{
+    Console.WriteLine("monday");
+}
+
+else if (day == 2)
+{
+    Console.WriteLine("thursday");
+}
+else
+{
+    Console.WriteLine("incorrect day");
+}
